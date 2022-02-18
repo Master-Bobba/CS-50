551905846497826b232bdc4adf822f3dec6346dc
Master-Bobba <99765912+Master-Bobba@users.noreply.github.com>
2022-02-18 22:51:10 +0000

    Add files via upload

---
+++ phonebook.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+// define a struct
+typedef struct{
+    string name;
+    string number;
+}
+person;
+
+int main(void){
+
+    person people[2]; //an array of struct people
+
+    people[0].name = "Carter";
+    people[0].number = "+1-617-495-1000";
+
+    people[1].name = "David";
+    people[1].number = "+1-949-468-2750";
+
+    for (int i = 0; i < 2; i++){
+
+        if (strcmp(people[i].name, "David") == 0){
+            printf("%s\n", people[i].number);
+            return 0;
+        }
+    }
+    printf("Wrong\n");
+    return 1;
+}
